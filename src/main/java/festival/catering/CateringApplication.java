b3309c16a8e7381dc2b4d074fb94301811227c2c
Vlad <vlad08052@mail.ru>
2024-11-10 22:05:28 +0100

    2

--- src/main/java/festival/catering/CateringApplication.java
+++ src/main/java/festival/catering/CateringApplication.java
@@ -3,14 +3,60 @@ package festival.catering;
 import org.salespointframework.EnableSalespoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableSalespoint
 @SpringBootApplication
 public class CateringApplication {
 
+	private static final String LOGIN_ROUTE = "/login";
+
 	public static void main(String[] args) {
 		SpringApplication.run(CateringApplication.class, args);
 	}
 
+	@Configuration
+	static class VideoShopWebConfiguration implements WebMvcConfigurer {
+
+		/**
+		 * We configure {@code /login} to be directly routed to the {@code login} template without any controller
+		 * interaction.
+		 *
+		 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurer#addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry)
+		 */
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController(LOGIN_ROUTE).setViewName("login");
+			registry.addViewController("/").setViewName("index");
+		}
+	}
+
+	@Configuration
+	@ConditionalOnWebApplication
+	static class WebSecurityConfiguration {
+
+		/**
+		 * Disabling Spring Security's CSRF support as we do not implement pre-flight request handling for the sake of
+		 * simplicity. Setting up basic security and defining login and logout options.
+		 *
+		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+		 */
+		@Bean
+		SecurityFilterChain videoShopSecurity(HttpSecurity http) throws Exception {
+
+			return http
+					.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+					.csrf(csrf -> csrf.disable())
+					.formLogin(login -> login.loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE))
+					.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/")).build();
+		}
+	}
 }
--- src/main/java/festival/catering/CateringController.java
+++ src/main/java/festival/catering/CateringController.java
@@ -1,10 +1,16 @@
 package festival.catering;
 
+import org.salespointframework.catalog.Product;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
+import org.salespointframework.order.Cart;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CateringController {
@@ -17,9 +23,23 @@ public class CateringController {
         this.inventory = inventory;
     }
 
-    @GetMapping("/catering")
-    String catering(Model model) {
+    @ModelAttribute("cart")
+    Cart initializeCart() {return new Cart();}
+
+    @GetMapping("/")
+    String catering(Model model, @ModelAttribute Cart cart) {
         model.addAttribute("catalog", cateringCatalog.findAll());
+        model.addAttribute("cart", cart);
         return "catering";
+    }
+
+    @PostMapping("/cart")
+    String addFood(@RequestParam("pid")Product.ProductIdentifier productId, @ModelAttribute Cart cart) {
+        Product product = cateringCatalog.findById(productId).orElseThrow();
+        if (product instanceof Food) {
+            Food food = (Food) product;
+            cart.addOrUpdateItem(food, Quantity.of(1));
+        }
+        return "redirect:/";
     }
 }
